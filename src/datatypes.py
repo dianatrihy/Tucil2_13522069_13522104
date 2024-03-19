cf5b372d09a31edae7a9f1f042cc6edab859bff5
Nabila Shikoofa <nabilashikoofa@gmail.com>
2024-03-19 12:46:00 +0700

    feat: datatype

--- src/datatypes.py
+++ src/datatypes.py
@@ -2,3 +2,17 @@ class Point:
     def __init__(self, x, y):
         self.x = x
         self.y = y
+
+class QuadraticBezier:
+    def __init__(self, points, iterations):
+        self.p0 = points[0]
+        self.p1 = points[1]
+        self.p2 = points[2]
+        self.iterations = iterations
+        self.elapsed_time = 0.0
+        
+class MultipointBezierDnC:
+    def __init__(self, points, iterations):
+        self.points = points # List of Point
+        self.iterations = iterations # integer
+        self.elapsed_time = 0.0
